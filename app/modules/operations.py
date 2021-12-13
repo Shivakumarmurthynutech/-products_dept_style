6129f1a0deea69768c20bf342ef911132afe029e
Shiva <shivakumar.murthy@nutechnologyinc.com>
2021-12-13 21:00:20 +0530

    try_block_Updated

--- app/modules/operations.py
+++ app/modules/operations.py
@@ -2,28 +2,47 @@ import pandas as pd
 from ..lib.constants import STG_MONGO_URL, PRD_MONGO_URL, DEV_MONGO_URL
 from ..lib.dbutils import runQuery
 from .mongo_queries import get_query
-# from ..lib.slackUtils import  send_slack_msg
 from ..lib.s3utils import writeToS3FromDataFrame
 from ..lib.constants import SLACK_ALERT_TEMPLATE
+from ..lib.slackUtils import  send_slack_msg
+from ..lib.constants import AWS_S3_BUCKET
+from ..lib.timeutils import getDateStr
 
-# slack_data = SLACK_ALERT_TEMPLATE.copy()
+
+slack_data = SLACK_ALERT_TEMPLATE.copy()
 
 def products_dept_style():
-    # slack_data['report'] = 'products_dept_style'
-    style = list(runQuery(get_query('styleAggregation'),DEV_MONGO_URL,'atomic_pim','productvariants'))
-    stylelist = []
-    for styleinfo in style:
-        for stl in styleinfo ['style']:
-            styledict = dict()
-            styledict['ProductId'] = styleinfo['_id']
-            styledict['Style'] = stl
-            styledict['Dept'] = styleinfo ['dept'][0]
-            stylelist.append(styledict)
-    report = pd.DataFrame(stylelist)
-    writeToS3FromDataFrame(report)
-        # slack_data['filename'] = 'productstyle'
-        # print("check")
-        # send_slack_msg(**report)
+    try:
+        slack_data['report'] = 'Products_dept_style'
+        style = list(runQuery(get_query('styleAggregation'),DEV_MONGO_URL,'atomic_pim','productvariants'))
+        stylelist = []
+        if style is not None:
+            for styleinfo in style:
+                if styleinfo is not None:
+                    for stl in styleinfo['style']:
+                        for dept in styleinfo['dept']:
+                            styledict = dict()
+                            styledict['ProductId'] = styleinfo['_id']
+                            styledict['Style'] = stl
+                            styledict['Dept'] = dept
+                            stylelist.append(styledict)
+            report = pd.DataFrame(stylelist)
+            date = getDateStr()
+            if writeToS3FromDataFrame(report) == 200:
+               slack_data['filename'] = f"products_dept_style_{date}.csv"
+               slack_data['bucket'] = AWS_S3_BUCKET
+               slack_data['total'] = len(report)
+               send_slack_msg(**slack_data)
+    except Exception as err:
+        print(f"Error in fetch-operations: {err}")
+        slack_data['alert_status'] = 'alert'
+        slack_data['error'] = err
+        send_slack_msg(**slack_data)
+
+
+
+
+
 
 
 
--- handler.py
+++ handler.py
@@ -1,8 +1,9 @@
 from app.modules.operations import products_dept_style
-
+from app.lib.timeutils import getDateStr
 
 if __name__ == '__main__':
     print("strated")
+    date = getDateStr()
     products_dept_style()
-    print("Finished Process \nOutput is in test.csv")
+    print(f"Output is in products_dept_style_{date}.csv")
     print("end")
