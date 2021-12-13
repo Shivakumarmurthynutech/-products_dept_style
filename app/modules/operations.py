bb02b9e9a909e93e11ef6e1a7743073ddc6fa673
Shiva <shivakumar.murthy@nutechnologyinc.com>
2021-12-13 14:32:49 +0530

    slack_notification

--- app/modules/operations.py
+++ app/modules/operations.py
@@ -2,20 +2,20 @@ import pandas as pd
 from ..lib.constants import STG_MONGO_URL, PRD_MONGO_URL, DEV_MONGO_URL
 from ..lib.dbutils import runQuery
 from .mongo_queries import get_query
-from ..lib.slackUtils import  send_slack_msg
+# from ..lib.slackUtils import  send_slack_msg
 from ..lib.s3utils import writeToS3FromDataFrame
 from ..lib.constants import SLACK_ALERT_TEMPLATE
 
 
 
-
-slack_data = SLACK_ALERT_TEMPLATE.copy()
+#
+# slack_data = SLACK_ALERT_TEMPLATE.copy()
 
 
 
 
 def products_dept_style():
-    slack_data['report'] = 'products_dept_style'
+    # slack_data['report'] = 'products_dept_style'
     style = list(runQuery(get_query('styleAggregation'),DEV_MONGO_URL,'atomic_pim','productvariants'))
     stylelist = []
     for styleinfo in style:
@@ -26,9 +26,10 @@ def products_dept_style():
             styledict['Dept'] = styleinfo ['dept'][0]
             stylelist.append(styledict)
     report = pd.DataFrame(stylelist)
-    if writeToS3FromDataFrame(report):
-        slack_data['filename'] = 'productstyle'
-        send_slack_msg(**report)
+    writeToS3FromDataFrame(report)
+        # slack_data['filename'] = 'productstyle'
+        # print("check")
+        # send_slack_msg(**report)
 
 
 
